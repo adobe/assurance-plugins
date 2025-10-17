207a4d781ea530d5b4fdaec949c828b12a22d8eb
Naman Samra <nsamra@adobe.com>
2025-10-18 01:38:30 +0530

    fix: client change handling

--- packages/ui-plugins/live-activities/src/App.tsx
+++ packages/ui-plugins/live-activities/src/App.tsx
@@ -11,8 +11,8 @@ import {
   Text
 } from '@adobe/react-spectrum';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import React from 'react';
-import { PluginBridgeProvider } from '@assurance/plugin-bridge-provider';
+import React, { useEffect } from 'react';
+import { PluginBridgeProvider, useSelectedClients } from '@assurance/plugin-bridge-provider';
 import Activities from './containers/activities';
 import Events from './containers/events';
 import ClientInfo from './components/validation/client-info';
@@ -50,7 +50,16 @@ function Inner() {
   const validationStatus = useLiveActivitiesValidationStatus();
   const messagingVersion = useClientMessagingVersion();
   const realActivities = useActivities();
+  const selectedClients = useSelectedClients();
+  const currentClient = selectedClients[0];
   const showActivitiesTab = (validationStatus === 'basic-support' || validationStatus === 'full-support') && messagingVersion && realActivities.length > 0;
+
+  useEffect(() => {
+    if (currentClient) {
+      setActiveTab(NAVIGATION_CONFIG.TOP_LEVEL_TABS.CLIENT_INFO);
+    }
+  }, [currentClient]);
+
 
   return (
     <View padding="size-200" paddingTop="size-0">
